f8df7a0ed6b220fb90c8991af216d90c7fed85e2
harunayyildiz <harunayyildizz15@gmai.com>
2020-02-09 06:22:21 +0300

    two load

--- lib/ImageSlider2.dart
+++ lib/ImageSlider2.dart
@@ -2,7 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 //carousel_slider: ^1.4.1 Kütüphanesini Kullandık
-//Scaffold Sabit Body Değişiyor...
+//Scaffold Sabit Body Değişiyor
 
 final List<String> imgList = [
   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
